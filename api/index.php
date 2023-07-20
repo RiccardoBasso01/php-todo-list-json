a005aba10bb465d6906725c2e17bfa5b2340e358
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-07-20 17:45:52 +0200

    Aggiunta chiamata post

--- api/index.php
+++ api/index.php
@@ -5,6 +5,21 @@ $json_data = file_get_contents($database_path);
 
 $actions = json_decode($json_data, true);
 
+// controlliamo se abbiamo qualcosa in post
+$new_action = $_POST['action'] ?? null;
+
+if ($new_action) {
+    $new_object = [
+        "text" => $new_action,
+        "done" => false
+    ];
+
+    $actions[] = $new_object;
+
+    $json_actions = json_encode($actions);
+    file_put_contents($database_path, $json_actions);
+}
+
 // Specifico che le informazioni sono di tipo json
 header('Content-Type: appclication/json');
 // Traduzione array in json
